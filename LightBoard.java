b1067cf95d888d79f5e946516dc2c388cb4934c6
Savannah-Lu <savannahlut@nycstudents.net>
2025-04-09 04:39:00 +0000

    slay

--- LightBoard.java
+++ LightBoard.java
@@ -1,48 +1,51 @@
 public class LightBoard{
+    /** The lights on the board, where true represents on and false represents off. */
+    private boolean[][] lights;
 
-/** The lights on the board, where true represents on and false represents off. */
-private boolean[][] lights;
-
-/** Constructs a LightBoard object having numRows rows and numCols columns.
-* Precondition: numRows > 0, numCols > 0
-* Postcondition: each light has a 40% probability of being set to on. */
-public LightBoard(int numRows, int numCols){ 
-    lights = new boolean[numRows][numCols];
-    for(int i=0; i<numRows ; i++){
-        for(int j=0; j<numCols ; j++){
-            if(0.40 >= Math.random()) lights[i][j] = true;
+    /** Constructs a LightBoard object having numRows rows and numCols columns.
+    * Precondition: numRows > 0, numCols > 0
+    * Postcondition: each light has a 40% probability of being set to on. */
+    public LightBoard(int numRows, int numCols){ 
+        lights = new boolean[numRows][numCols];
+        for(int i=0; i<numRows ; i++){
+            for(int j=0; j<numCols ; j++){
+                if(0.40 >= Math.random()) lights[i][j] = true;
+            }
         }
     }
-}
 
-/** Evaluates a light in row index row and column index col and returns a status
-* as described in part (b).
-* Precondition: row and col are valid indexes in lights.
-*/
-public boolean evaluateLight(int row, int col){
-    boolean currentLight = lights[row][col];
-    int on = 0;
-    for(int i=0; i<lights.length ; i++){
-        if (currentLight) on++;
+    /** Evaluates a light in row index row and column index col and returns a status
+    * as described in part (b).
+    * Precondition: row and col are valid indexes in lights.
+    */
+    public boolean evaluateLight(int row, int col){
+        boolean currentLight = lights[row][col];
+        int on = 0;
+        for(int i=0; i<lights.length ; i++){
+            if (lights[i][col]) on++;
+        }
+        if(currentLight){
+            if(on % 2 == 0) return false;
+        }
+        if(!currentLight){
+            if(on % 3 == 0) return true;
+        }
+        return currentLight;
     }
-    if(currentLight){
-        if(on % 2 == 0) return false;
-    }
-    if(!currentLight){
-        if(on % 3 == 0) return true;
-    }
-    return currentLight;
-}
 
 // There may be additional instance variables, constructors, and methods not shown.
-public String toString(){
-    String result = "[ " ;
-    for(boolean[] r : lights){
-        for(boolean c : r){
-            result += c + ", " ;
+    public String toString(){
+        String result = "[ " ;
+        for(boolean[] r : lights){
+            for(boolean c : r){
+                result += c + ", " ;
+            }
+            result += "\n" ;
         }
-        result += "\n" ;
+        return result + " ]";
     }
-    return result + " ]";
-}
+
+    public void setlights(boolean[][] newlist){
+        lights = newlist;
+    }
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,19 @@ public class Main{
         LightBoard LB1 = new LightBoard(5, 5);
         System.out.println(LB1);
 
-        
+        LightBoard LBJ = new LightBoard(7, 5);
+        LBJ.setlights(
+        new boolean[][]{{true, true, false, true, true},
+                        {true, false, false, true, false},
+                        {true, false, false, true, true},
+                        {true, false, false, false, true},
+                        {true, false, false, false, true},
+                        {true, true, false, true, true},
+                        {false, false, false, false, false}});
+        System.out.println(LBJ);  
+        System.out.println(LBJ.evaluateLight(0, 3));
+        System.out.println(LBJ.evaluateLight(6, 0));
+        System.out.println(LBJ.evaluateLight(4, 1));
+        System.out.println(LBJ.evaluateLight(5, 4));
     }
 }
